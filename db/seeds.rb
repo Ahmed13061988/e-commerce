8d4d311b8adeee1ecfff0bc9e7cc7ee7c3f1a991
Ahmed <wolktheline@yahoo.com>
2020-12-07 16:08:12 -0700

    adding seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+items.create(
+                name:"Sunglasses",
+                description:"The trendiest designs for your face.",
+                image:"https://source.unsplash.com/SYx3UCHZJlo/250x250",
+                price: 125
+            )
+
+
+            user_1 = User.create(
+                first_name: "Ahmed",
+                last_name: "Hussein",
+                email:"ahmed@yahoo.com",
+                password: "1",
+                password_confirmation: "1"
+            )
+
+
+            cart = user_1.carts.create
+
+            cart.items << Item.first
+            cart.save
