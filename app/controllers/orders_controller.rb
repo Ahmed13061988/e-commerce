cf3e8c63e79c0a0ec9ea5e799b07c8fab0437f2a
Ahmed <wolktheline@yahoo.com>
2020-12-19 15:35:22 -0700

    done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,10 @@ class OrdersController < ApplicationController
     def create
         
             #    byebug 
-            #  item = Item.find(params["item"]["id"])
-            # cart = Cart.find_by(params[:cart_id])
              current_cart = Cart.all
              order_items = current_cart.last.items
             #  a = order_items.map{|item| item } 
                a = order_items.each { |key, value| puts "k: #{key}, v: #{value}" }
-             byebug
-            #  a = order_items.each do |item| 
 
              
             
@@ -17,17 +13,7 @@ class OrdersController < ApplicationController
 
              
             
-            # # item =current_cart.cart_items.find_by(item_id: params["item"]["itemId"])
-            # params[:cart_id]= current_cart
-            # order.cart_id = params[:cart_id]
-            # order.save
-
-            # # byebug
-            #  order_items = []
-            #  order_items.push(current_cart.items)
-            # # order.cart = current_cart
-            # # order.save
-            #   byebug
+        
              setup_new_cart
             #  byebug
         
@@ -36,11 +22,7 @@ class OrdersController < ApplicationController
             order: {
                 id:  current_cart,
                 items: a 
-                 }
-                # total: current_cart ?  current_cart_total : 0
-                # id: current_cart.id,
-                # items: current_cart.items
-         }
+                 } }
         
     end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :image, :name, :price, :description
-  has_many :carts 
-  # belongs_to :order 
+  has_many :carts
 end
